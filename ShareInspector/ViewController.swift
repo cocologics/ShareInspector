9974bc6e72639c3fb72531324ab04a7ac536fca2
Ole Begemann <ole@oleb.net>
2019-07-22 12:35:20 +0200

    Clean up files generated by Xcode project template

--- ShareInspector/ViewController.swift
+++ ShareInspector/ViewController.swift
@@ -1,20 +1,7 @@
-//
-//  ViewController.swift
-//  ShareInspector
-//
-//  Created by Ole Begemann on 22.07.19.
-//  Copyright © 2019 Ole Begemann. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
-
-
 }
-
