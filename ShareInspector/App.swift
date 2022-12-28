9c63e9a4a23a5aa8a2d9bc71f7cce2b1d38aadf4
Ole Begemann <ole@oleb.net>
2022-12-28 19:41:51 +0100

    Switch to SwiftUI.App-based launch cycle

---
+++ ShareInspector/App.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct ShareInspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+              .tint(Color("pcTintColor"))
+        }
+    }
+}
