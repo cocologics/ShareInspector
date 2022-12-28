fdcdc19ac5f4f85ce615c4fd779421ac29325497
Ole Begemann <ole@oleb.net>
2022-12-28 19:32:57 +0100

    Raise deployment target to iOS 16.0

--- ShareInspectorModel/Package.swift
+++ ShareInspectorModel/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "ShareInspectorModel",
-  platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
+  platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9)],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
     .library(
